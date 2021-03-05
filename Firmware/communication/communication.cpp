f9c47cfaaff8a7d2011996e2b6b0ddec59c005f8
labmrd <labmrd@gmail.com>
2021-03-05 13:40:08 -0600

    definition and communication

--- Firmware/communication/communication.cpp
+++ Firmware/communication/communication.cpp
@@ -79,7 +79,7 @@ auto make_protocol_definitions(PWMMapping_t& mapping) {
 }
 
 /* ERG - Sub-tree for charData access -----------------------------------------------*/
-/*
+
 auto make_charData_definitions(size_t idx) {
     return make_protocol_member_list(
         make_protocol_property("timestep", &charData[0][idx]),
@@ -88,7 +88,7 @@ auto make_charData_definitions(size_t idx) {
         make_protocol_property("vel", &charData[3][idx])
     );
 }
-*/
+
 
 /* Function implementations --------------------------------------------------*/
 
@@ -108,9 +108,9 @@ float oscilloscope[OSCILLOSCOPE_SIZE] = {0};
 size_t oscilloscope_pos = 0;
 
 // ERG - modeling characterization data structure after oscilloscope
-////CharData_t charData[CHARDATA_SIZE] = {0}; ERG TODO - either uncomment or delete
-//float charData[4][CHARDATA_SIZE] = {0}; //the 4x128 version
-//size_t charData_pos = 0;
+//CharData_t charData[CHARDATA_SIZE] = {0}; ERG TODO - either uncomment or delete
+float charData[4][CHARDATA_SIZE] = {0}; //the 4x128 version
+size_t charData_pos = 0;
 
 static CAN_context can1_ctx;
 
@@ -187,7 +187,7 @@ static inline auto make_obj_tree() {
         make_protocol_object("axis0", axes[0]->make_protocol_definitions()),
         make_protocol_object("axis1", axes[1]->make_protocol_definitions()),
         make_protocol_object("can", can1_ctx.make_protocol_definitions()),
-        //make_protocol_object("charData", make_charData_definitions(charData_pos)), //ERG - allows odrivetool to access charData
+        make_protocol_object("charData", make_charData_definitions(charData_pos)), //ERG - allows odrivetool to access charData
         make_protocol_property("test_property", &test_property),
         make_protocol_function("test_function", static_functions, &StaticFunctions::test_function, "delta"),
         make_protocol_function("get_oscilloscope_val", static_functions, &StaticFunctions::get_oscilloscope_val, "index"),
